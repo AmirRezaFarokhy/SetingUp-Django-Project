813ddadb54ee7fafe4a9da756f6758912e700053
AmirReza Farokhy <113052872+AmirRezaFarokhy@users.noreply.github.com>
2023-08-02 18:01:41 +0330

    Update celery.py

--- celery.py
+++ celery.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bycile.settings')
-app = Celery('bycile')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YOUR_CELERY_NAME.settings')
+app = Celery('YOUR_CELERY_NAME')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
